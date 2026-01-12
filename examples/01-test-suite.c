c82fc7975af83ce982de6f47efd0de19880a2264
Subhajit Sahu <wolfram77@gmail.com>
2026-01-12 21:34:03 +0530

    :bug: with examples

---
+++ examples/01-test-suite.c
@@ -0,0 +1,66 @@
+#define CLOVE_IMPLEMENTATION
+#include <clove-unit.h>
+
+CLOVE_RUNNER()
+
+
+
+#define CLOVE_SUITE_NAME MySuite01
+
+CLOVE_TEST(BooleanTest) {
+  CLOVE_IS_TRUE(1);
+}
+
+CLOVE_TEST(CharTest) {
+  CLOVE_CHAR_EQ('a', 'a');
+  CLOVE_CHAR_NE('a', 'b');
+}
+
+CLOVE_TEST(IntTest) {
+  CLOVE_INT_EQ(2-1, 1);
+}
+
+CLOVE_TEST(UIntTest) {
+  unsigned int value = 1;
+  CLOVE_UINT_EQ(value, 1u);
+}
+
+CLOVE_TEST(LongfamilyTest) {
+  long l = 1;
+  long long ll = 1;
+  unsigned long ul = 1;
+  unsigned long long ull = 1;
+
+  CLOVE_LONG_EQ(l, 1L);
+  CLOVE_LLONG_EQ(ll, 1LL);
+  CLOVE_ULONG_EQ(ul, 1UL);
+  CLOVE_ULLONG_EQ(ull, 1ULL);
+}
+
+CLOVE_TEST(FloatTest) {
+  //By default two floats are equals if their diff is less then E-6
+  CLOVE_FLOAT_NE(1.0f, 1.0001f);
+  CLOVE_FLOAT_EQ(1.0f, 1.0001f);
+}
+
+CLOVE_TEST(DoubleTest) {
+  //By default two double are equals if their diff is less then E-15
+  CLOVE_DOUBLE_NE(1.0, 1.0001);
+  CLOVE_DOUBLE_EQ(1.0, 1.000000000000001);
+}
+
+CLOVE_TEST(StringTest) {
+  CLOVE_STRING_NE("123", "1234");
+  CLOVE_STRING_EQ("123", "123");
+
+  const char array[] = {'1', '2', '3', '\0'};
+  CLOVE_STRING_EQ(array, "123");
+}
+
+CLOVE_TEST(PassTest) {
+  CLOVE_PASS();
+}
+
+CLOVE_TEST(FailTest) {
+  CLOVE_FAIL();
+}
---
+++ examples/03-test-suite.c
@@ -0,0 +1,35 @@
+#define CLOVE_IMPLEMENTATION
+#include <clove-unit.h>
+
+CLOVE_RUNNER()
+
+
+
+#define CLOVE_SUITE_NAME MySuite03
+
+CLOVE_TEST(FailingTest) {
+  int a = 1;
+  int b = 2;
+  CLOVE_INT_EQ(a, b);
+}
+
+/*
+CLOVE_TEST(CheckNotNull) {
+  int* null_ptr = NULL;
+  CLOVE_NOT_NULL(null_ptr);
+}
+
+
+CLOVE_TEST(CheckNull) {
+  int myvalue = 1;
+  CLOVE_NULL(&myvalue);
+}
+*/
+
+CLOVE_TEST(StringFailTest) {
+  CLOVE_STRING_EQ("Hello", "World");
+}
+
+CLOVE_TEST(SkippedTest) {
+
+}
